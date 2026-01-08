87691ae38148a90990b4f2dd9edd74539e79c2e9
earthscience2 <slhg@hanmail.net>
2026-01-08 16:24:48 +0900

    clean 3

--- pole/Lib/site-packages/magpylib/current/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This subpackage contains all electric current classes. Currents are modeled as line-currents,
-input is the current in units of Ampere [A]. Field computation formulas are obtained via the law of
-Biot-Savardt.
-"""
-
-__all__ = ['Circular', 'Line']
-
-from magpylib._lib.obj_classes import Circular, Line
--- pole/Lib/site-packages/magpylib/misc/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This sub-package contains miscellaneous source objects.
-"""
-
-__all__ = ['Dipole']
-
-from magpylib._lib.obj_classes import Dipole
